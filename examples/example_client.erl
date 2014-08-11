324bfcdabdea7350e322a5b2c7213844b0a57209
Stanislaw Klekot <dozzie@jarowit.net>
2014-08-11 18:56:47 +0200

    Added an example client module.

---
+++ examples/example_client.erl
@@ -0,0 +1,107 @@
+%%%---------------------------------------------------------------------------
+%%% @doc
+%%%   Example IRC client.
+%%%
+%%%   Usage (to be simplified):
+%%%   ```
+%%%   {ok, Pid} = example_client:connect("chat.example.net", 6667, "mynick"),
+%%%   {ok, JoinCmd} = ealirc_proto:join(["#atled"]),
+%%%   gen_ealirc:quote(Pid, JoinCmd).
+%%%   '''
+%%% @end
+%%%---------------------------------------------------------------------------
+
+-module(example_client).
+
+-behaviour(gen_ealirc).
+
+%%% public API
+-export([connect/3]).
+
+%%% gen_ealirc callbacks
+-export([init/1, terminate/2]).
+-export([handle_call/3, handle_cast/2, handle_info/2, handle_message/4]).
+-export([code_change/3]).
+
+%%%---------------------------------------------------------------------------
+
+-record(state, {nick}).
+
+%%%---------------------------------------------------------------------------
+%%% public API
+
+connect(Server, Port, Nick) ->
+  gen_ealirc:connect(Server, Port, ?MODULE, [Nick], []).
+
+%%%---------------------------------------------------------------------------
+%%% gen_ealirc callbacks
+
+%%----------------------------------------------------------
+%% initialization and cleanup {{{
+
+%% @private
+%% @doc Initialize {@link gen_ealirc} state.
+
+init([Nick] = _Args) ->
+  {ok, NickCmd} = ealirc_proto:nick(Nick),
+  {ok, UserCmd} = ealirc_proto:user(Nick, none, Nick),
+  gen_ealirc:quote(NickCmd),
+  gen_ealirc:quote(UserCmd),
+  {ok, #state{nick = Nick}}.
+
+%% @private
+%% @doc Clean up {@link gen_ealirc} state.
+
+terminate(_Reason, _State) ->
+  ok.
+
+%% }}}
+%%----------------------------------------------------------
+%% communication {{{
+
+%% @private
+%% @doc Handle {@link gen_server:call/2}.
+
+handle_call(_Request, _From, State) ->
+  {reply, {error, unknown}, State}.
+
+%% @private
+%% @doc Handle {@link gen_server:cast/2}.
+
+handle_cast(_Request, State) ->
+  {noreply, State}.
+
+%% @private
+%% @doc Handle incoming messages.
+
+handle_info(_Msg, State) ->
+  {noreply, State}.
+
+%% @private
+%% @doc Handle incoming IRC messages.
+
+handle_message(Prefix, "PING" = _Command, Args, State = #state{nick = Nick}) ->
+  {ok, PongCmd} = ealirc_proto:pong(Nick),
+  gen_ealirc:quote(PongCmd),
+  io:fwrite("PING(~1024p) from ~1024p~n", [Args, Prefix]),
+  {noreply, State};
+
+handle_message(Prefix, Command, Args, State) ->
+  io:fwrite("[~p] ~p ~1024p~n", [Prefix, Command, Args]),
+  {noreply, State}.
+
+%% }}}
+%%----------------------------------------------------------
+%% code change {{{
+
+%% @private
+%% @doc Handle code change.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%% }}}
+%%----------------------------------------------------------
+
+%%%---------------------------------------------------------------------------
+%%% vim:ft=erlang:foldmethod=marker
